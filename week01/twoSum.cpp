7e51b1e3f06959cce279eca506296f8c64277865
Junaid Mansur Ifti <46519975+bsse1027@users.noreply.github.com>
2022-04-09 01:13:59 +0600

    Create twoSum.cpp

---
+++ week01/twoSum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        unordered_map<int , int> visited;
+        int newTarget;
+        
+        for(int i=0 ; i<nums.size(); i++)
+        {
+            
+            newTarget = target - nums[i];
+            if(visited.count(newTarget))
+            {
+                return {i , visited[newTarget]} ;
+            }
+            visited[nums[i]] = i ;
+        }
+        
+        return {} ;
+        
+    }
+};
