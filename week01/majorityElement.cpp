db2f10ca4e5bd1728a2ab28dcc939d6fb7a11be2
Junaid Mansur Ifti <46519975+bsse1027@users.noreply.github.com>
2022-04-09 01:15:40 +0600

    Create majorityElement.cpp

---
+++ week01/majorityElement.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int relativeVote=0;
+        int winner=nums[0];
+        
+        for(int i=0; i<nums.size();i++)
+        {
+            
+            if(nums[i] == winner) relativeVote++;
+            else{
+                relativeVote--;
+            }
+            
+            if(relativeVote == 0) {
+                winner=nums[i];
+                relativeVote++;
+            }
+        
+        }
+        
+        return winner;
+        
+    }
+};
