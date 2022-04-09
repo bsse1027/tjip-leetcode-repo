90257c646158377743008b083707c4a4d9672df9
Junaid Mansur Ifti <46519975+bsse1027@users.noreply.github.com>
2022-04-10 03:32:48 +0600

    Create moveZeros.cpp

---
+++ week01/moveZeros.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        for(int i=0,j=0; i<nums.size();i++){
+            
+            if(nums[i]==0) continue;
+            
+            else if(nums[j]==0){
+                nums[j]=nums[i];
+                nums[i]=0;
+                j++;
+            }
+            
+            else j++;
+            }
+        }
+};
