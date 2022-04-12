7b0722366d163967e1cb510b1b1962e22f3938ad
Junaid Mansur Ifti <46519975+bsse1027@users.noreply.github.com>
2022-04-13 02:03:53 +0600

    Update moveZeros.cpp

--- week01/moveZeros.cpp
+++ week01/moveZeros.cpp
@@ -5,13 +5,11 @@ public:
         for(int i=0,j=0; i<nums.size();i++){
             
             if(nums[i]==0) continue;
-            
             else if(nums[j]==0){
                 nums[j]=nums[i];
                 nums[i]=0;
                 j++;
-            }
-            
+            } 
             else j++;
             }
         }
